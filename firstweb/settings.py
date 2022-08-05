09da1287b624b325412aa45229068e8eda62e56c
Pongpun364 <pongpun364@hotmail.com>
2022-08-05 22:35:05 +0700

    before resolving the missing env variable

--- firstweb/settings.py
+++ firstweb/settings.py
@@ -22,8 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 HASHIDS_SALT = os.environ.get('HASHIDS_SALT')
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == "1" # 1 == True
+
 LINE_CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
 LINE_CHANNEL_SECRET = os.environ.get('LINE_CHANNEL_SECRET')
 ALLOWED_HOSTS = ['*']
